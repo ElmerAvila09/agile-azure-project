75ef7b11eba75c1595541929cbd86797a9cb45c6
Elmer Avila <62662713+ElmerAvila09@users.noreply.github.com>
2023-10-27 15:48:48 +0200

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://agilewebapp243269.azurewebsites.net:$PORT/predict 
+     -X POST https://agilewebapp243371.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
